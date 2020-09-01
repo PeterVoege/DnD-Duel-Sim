b9d807ac1ae2e8ef30d957a1317b72d5fa0fe875
Peter Voege <PeterVoege2@gmail.com>
2020-09-01 13:26:22 -0400

    Character interface and Fighter class
    Only includes attributes currently.

---
+++ DnD Duel Sim/Fighter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Duel_Sim
+{
+    class Fighter : ICharacter
+    {
+        private int _str;
+        private int _dex;
+        private int _con;
+        private int _int;
+        private int _wis;
+        private int _cha;
+
+        public int GetStr() => _str;
+        public int GetDex() => _dex;
+        public int GetCon() => _con;
+        public int GetInt() => _int;
+        public int GetWis() => _wis;
+        public int GetCha() => _cha;
+
+        public void SetStr(int Str) => _str = Str;
+        public void SetDex(int Dex) => _dex = Dex;
+        public void SetCon(int Con) => _con = Con;
+        public void SetInt(int Int) => _int = Int;
+        public void SetWis(int Wis) => _wis = Wis;
+        public void SetCha(int Cha) => _cha = Cha;
+
+        public int GetStrMod() => (_str - 10) / 2;
+        public int GetDexMod() => (_dex - 10) / 2;
+        public int GetConMod() => (_con - 10) / 2;
+        public int GetIntMod() => (_int - 10) / 2;
+        public int GetWisMod() => (_wis - 10) / 2;
+        public int GetChaMod() => (_cha - 10) / 2;
+    }
+}
---
+++ DnD Duel Sim/ICharacter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Duel_Sim
+{
+    interface ICharacter
+    {
+        int GetStr();
+        int GetDex();
+        int GetCon();
+        int GetInt();
+        int GetWis();
+        int GetCha();
+
+        void SetStr(int Str);
+        void SetDex(int Dex);
+        void SetCon(int Con);
+        void SetInt(int Int);
+        void SetWis(int Wis);
+        void SetCha(int Cha);
+
+        int GetStrMod();
+        int GetDexMod();
+        int GetConMod();
+        int GetIntMod();
+        int GetWisMod();
+        int GetChaMod();
+    }
+}
