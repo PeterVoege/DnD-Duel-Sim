e8e6a24ee98b61c460cacec3a5ca5509ce0c23a2
Peter Voege <PeterVoege2@gmail.com>
2020-09-04 13:41:12 -0400

    Added 'Duel Setup Form'

---
+++ DnD Duel Sim/DuelSetupForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DnD_Duel_Sim
+{
+    public partial class DuelSetupForm : Form
+    {
+        public DuelSetupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fighter_Generator fighter1Form = new Fighter_Generator();
+            fighter1Form.Show();
+        }
+    }
+}
